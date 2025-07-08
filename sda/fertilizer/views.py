2f643f73d123e544ac3d661de79cf4d906f12162
haribari01 <hajong0625@gmail.com>
2025-07-08 11:43:50 +0900

    7.8

--- sda/fertilizer/views.py
+++ sda/fertilizer/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def prescription(request):
+    return render(request, 'fertilizer/prescription.html')
+
+def experience(request):
+    return render(request, 'fertilizer/experience.html')
+
+def standard(request):
+    return render(request, 'fertilizer/standard.html')
--- sda/water/views.py
+++ sda/water/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def water(request):
+    return render(request, 'water/water.html')
